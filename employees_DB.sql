63f5368bfb539c3147b58af7a2d327ceaca1c91d
Panthelya <116551755+Panthelya@users.noreply.github.com>
2022-10-24 10:48:36 +0200

    Add files via upload

---
+++ employees_DB.sql
@@ -0,0 +1,25 @@
+CREATE TABLE employees (
+	id INT, 
+	first_name VARCHAR (50),
+	last_name VARCHAR (50),
+	job_title VARCHAR (50),
+	department VARCHAR (50),
+	date_of_employment DATE, 
+	salary DECIMAL (10,2)
+);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (1, 'Bob', 'Mendelsohn', 'Data Analyst', 'Risk Management', '2020_09_25', 5412.47);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (2, 'Frances', 'Jackson', 'Data Analyst', 'Marketing', '2020_04_24', 4919.34);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (3, 'Franck', 'Monteblanc', 'Data Scientist', 'Marketing', '2021_03_18', 7519.34);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (4, 'Patricia', 'King', 'Data Scientist', 'Risk Management', '2020_03_05', 7871.69);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (5, 'Willie', 'Hayes', 'Statistician', 'Risk Management', '2021_07_09', 6995.87);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (6, 'Simone', 'Hill', 'Statistician', 'Marketing', '2021_05_09',6815.67);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (7, 'Walter', 'Tyson', 'Database Administrator', 'IT', '2022_08_12', 7512.14);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (8, 'Ines', 'Owen', 'Database Administrator', 'IT', '2021_09_15', 8105.41);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (9, 'Carolina', 'Oliveira', 'Data Engineer', 'IT', '2022_09_15', 8410.57);
+INSERT INTO employees (id, first_name, last_name, job_title, department, date_of_employment, salary) VALUES (10, 'Sean', 'Rice', 'System Analyst', 'IT', '2022_01_19', 6518.22);
+
+
+
+
+
+
