67a2ddcbe3af22adc46be693325c6384d4959eeb
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-03-28 08:43:56 +0200

    Add files via upload

---
+++ books_DB.sql
@@ -0,0 +1,28 @@
+CREATE TABLE books (
+	id INT PRIMARY KEY,
+	title VARCHAR (120),
+	author VARCHAR (120),
+	original_language VARCHAR (120),
+	sales INT
+
+	
+);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (1, 'The Hobbit', 'J. R. R. Tolkien', 'English', 100);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (2, 'Watership Down', 'Richard Adams', 'English', 50);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (3, 'Harry Potter and the Philosopher''s Stone', 'J. K. Rowling', 'English', 120);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (4, 'The Plague', 'Albert Camus', 'French', 12);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (5, 'The Divine Comedy', 'Dante Alighieri', 'Italian', 12);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (6, 'War And Peace', 'Leo Tolstoy', 'Russian', 36);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (7, 'Nineteen Eighty-Four', 'George Orwell', 'English', 30);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (8, 'Andromeda Nebula', 'Ivan Yefremov', 'Russian', 20);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (9, 'The Little Prince', 'Antoine de Saint-Exupéry', 'French', 200);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (10, 'The Stranger', 'Albert Camus', 'French', 10);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (11, 'The Adventures of Pinocchio', 'Carlo Collodi', 'Italian', 35);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (12, 'The Name of the Rose', 'Umberto Eco', 'Italian', 50);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (13, 'One Hundred Years of Solitude', 'Gabriel García Márquez', 'Spanish', 50);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (14, 'Don Quixote', 'Miguel de Cervantes', 'Spanish', 500);
+INSERT INTO books (id, title, author, original_language, sales) VALUES (15, 'Lolita', 'Vladimir Nabokov', 'English', 50);
+
+
+
+
